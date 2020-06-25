52b8c7c28c8c020b536db3835f8fdcc394ffca40
Nikolay <shadowapril12@gmail.com>
2020-06-25 18:46:07 +0300

    Initial commit

---
+++ CountingLib.UnitTests/CountingLib.UnitTests/SumMakerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using CountingLib;
+using NUnit.Framework;
+
+namespace CountingLib.UnitTests
+{
+    [TestFixture]
+    public class SumMakerTests
+    {
+        [Test]
+        public void Sum_Arg1Equal4Arg2Equal5_ReturnsIntegerResultEquals9()
+        {
+            // Arrange
+            SumMaker sumMaker = new SumMaker();
+
+            // Act
+            int res = sumMaker.Sum(4, 5);
+
+            // Assert
+            Assert.AreEqual(9, res);
+        }
+    }
+}
