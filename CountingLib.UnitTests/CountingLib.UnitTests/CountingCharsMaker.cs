e3f76e731e24e34ba44a8de9dc38ffd5f031a2fc
Nikolay <shadowapril12@gmail.com>
2020-06-26 09:45:45 +0300

    Added test for counting chars

---
+++ CountingLib.UnitTests/CountingLib.UnitTests/CountingCharsMaker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace CountingLib.UnitTests
+{
+    [TestFixture]
+    public class CountingCharsMaker
+    {
+        [Test]
+        public void Count_InputWordWorld_ReturnsIntegerResultEquals5()
+        {
+            // Arrange
+            string testWord = "world";
+            CharCounter charCounter = new CharCounter();
+
+            // Action
+            int result = charCounter.GetCountOfChars(testWord);
+
+            // Assert
+            Assert.AreEqual(result, 5);
+        }
+    }
+}
